0a7b24552ffa7f6f0ee8e75b97bc7e1956b87810
Lilian Ly <lilian.ly@anz.com>
2023-04-12 11:45:35 +1000

    added basic variables in the view model for the flow state of user searching for train station names

--- app/src/main/java/com/example/ptvproject/ui/selecttrainstation/SelectTrainStationScreen.kt
+++ app/src/main/java/com/example/ptvproject/ui/selecttrainstation/SelectTrainStationScreen.kt
@@ -1,3 +1,24 @@
 package com.example.ptvproject.ui.selecttrainstation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
 // generate list of train stations
+// the ui here handles the search bar, keyboard and generation/return of results
+
+@Composable
+fun SelectTrainStationScreen() {
+
+    // this composable should contain the combined overall layout for the page
+    // it should refresh when we change from the search bar?
+}
+
+@Composable
+fun SearchForTrainStation(searchText: String) {
+
+    // composable with empty rounded search bar, bring up keyboard
+
+
+
+
+}
--- app/src/main/java/com/example/ptvproject/ui/selecttrainstation/SelectTrainStationViewModel.kt
+++ app/src/main/java/com/example/ptvproject/ui/selecttrainstation/SelectTrainStationViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.ptvproject.ui.selecttrainstation
 
-class SelectTrainStationViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SelectTrainStationViewModel: ViewModel() {
+
+    // ensures that the VM can change the mutable one, but the UI should not be able to change it
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    // allow us to hide or make visible the search functionality
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching = _isSearching.asStateFlow()
+    
 }
+
+/* Logic here: the user selects a train station using a search pad and inputting
+* a String that is then matched to the station name. The VM retains the train station data/name */
