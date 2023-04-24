51fac5feb78c4c68d65876061da4abf59782c3a8
Lilian Ly <lilian.ly@anz.com>
2023-04-24 11:47:55 +1000

    Added mutable notification state in NotificationScreen

--- app/src/main/java/com/example/ptvproject/ui/notification/NotificationViewModel.kt
+++ app/src/main/java/com/example/ptvproject/ui/notification/NotificationViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.ptvproject.ui.notification
 
-class NotificationViewModel(val stationId: Int, val trainLineId: Int) {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class NotificationViewModel() : ViewModel() {
+
+    private val _notificationsUiState:
+            MutableStateFlow<NotificationsUiState> =
+        MutableStateFlow(NotificationsUiState())
+
+    val notificationsUiState: StateFlow<NotificationsUiState> =
+        _notificationsUiState.asStateFlow()
+
+    // need to take inputs from previous screens to display
+
+    // Do I need this function because I have not provided the parameters previously?
+    private fun NotificationsUiState(): NotificationsUiState {
+
+
+    }
+
+
+    //https://developer.android.com/codelabs/basic-android-kotlin-compose-viewmodel-and-state#4
+
+    // Here, we will contain the logic for the notifications
+
+    // function to calculate whether the user is on time or not
+
+    // function to take/pass inputs and display them on the screen
+
 }
