fe2ad1a5c089c04ee8daa475fce8b139bcccb2e9
Lilian Ly <lilian.ly@anz.com>
2023-04-24 11:47:55 +1000

    Added mutable notification state in NotificationScreen

--- app/src/main/java/com/example/ptvproject/ui/notification/NotificationScreen.kt
+++ app/src/main/java/com/example/ptvproject/ui/notification/NotificationScreen.kt
@@ -1,11 +1,12 @@
 package com.example.ptvproject.ui.notification
 
 import androidx.compose.runtime.Composable
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 
 @Composable
-private fun Notification(stationId: Int, trainLineId: Int){}
+fun NotificationScreen(stationId: Int, trainLineId: Int){}
 
 @Composable
-fun Notification(vM: NotificationViewModel){}
+private fun NotificationScreen(vM: NotificationViewModel){}
+
+// two parts to the screen. top = user, bottom = train
